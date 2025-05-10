67eec015c0ebdd175b89f9407ad4209ca79779f2
채근영 <chaulfe@gmail.com>
2025-05-10 20:52:54 +0900

    refactor: main.js 파일 분리

---
+++ src/js/highlight.js
@@ -0,0 +1,15 @@
+import hljs from "highlight.js/lib/core";
+import python from "highlight.js/lib/languages/python";
+import xml from "highlight.js/lib/languages/xml"; // HTML용
+import javascript from "highlight.js/lib/languages/javascript";
+import css from "highlight.js/lib/languages/css";
+import "highlight.js/styles/atom-one-dark.css";
+
+hljs.registerLanguage("python", python);
+hljs.registerLanguage("html", xml);
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("css", css);
+
+document.addEventListener("DOMContentLoaded", () => {
+  hljs.highlightAll();
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,2 @@
 import "/styles/style.scss";
-
-import hljs from "highlight.js/lib/core";
-import python from "highlight.js/lib/languages/python";
-import xml from "highlight.js/lib/languages/xml"; // HTML용
-import javascript from "highlight.js/lib/languages/javascript";
-import css from "highlight.js/lib/languages/css";
-import "highlight.js/styles/atom-one-dark.css";
-
-hljs.registerLanguage("python", python);
-hljs.registerLanguage("html", xml);
-hljs.registerLanguage("javascript", javascript);
-hljs.registerLanguage("css", css);
-
-document.addEventListener("DOMContentLoaded", () => {
-  hljs.highlightAll();
-});
+import "/styles/login.scss";
