9bc6ff97234e6be20e0fd4ec98c5e905aa4d88c4
채근영 <chaulfe@gmail.com>
2025-05-13 15:12:59 +0900

    fix: 웹기초 탭 접근 불가 메세지 오류 수정

--- src/js/webBasic.js
+++ src/js/webBasic.js
@@ -1,7 +1,8 @@
 const webBasicBtn = document.querySelector(".web-basic-btn");
 
 if (webBasicBtn) {
-  webBasicBtn.addEventListener("click", () => {
+  webBasicBtn.addEventListener("click", (event) => {
+    event.preventDefault();
     alert("웹 기초 기능은 추후 추가될 예정입니다.");
   });
 }
