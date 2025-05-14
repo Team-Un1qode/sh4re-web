84acf8c5918e78c67f423c54752dcec25cf1f0c3
채근영 <chaulfe@gmail.com>
2025-05-14 18:00:25 +0900

    fix: 게스트 상태 write페이지 접근 시 리다이렉션 alert 추가

--- src/js/write.js
+++ src/js/write.js
@@ -1,6 +1,9 @@
-import {customFetch, getCookie} from "/js/common.js";
+import { customFetch, getCookie } from "/js/common.js";
 
-if(!getCookie("accessToken")) document.location.href = "/";
+if (!getCookie("accessToken")) {
+  alert("로그인 후 접근해주세요.");
+  document.location.href = "/";
+}
 
 const title = document.querySelector(".title");
 const code = document.querySelector(".code");
@@ -8,29 +11,29 @@ const shareBtn = document.querySelector(".share");
 let loading = false;
 
 const handleSubmit = async (e) => {
-    console.log(title.value, code.value)
-    e.preventDefault();
-    if(loading) return;
-    if(!title.value || !code.value) return;
-    try {
-        loading = true;
-        shareBtn.innerText = "로딩 중..";
-        const res = await customFetch("/codes", {
-            method: "POST",
-            body: {
-                title: title.value,
-                code: code.value,
-                field: "PYTHON"
-            }
-        })
-        if(res.ok && res.data){
-            alert("코드 작성에 성공하였습니다.")
-            document.location.href = `/code?id=${res.data.id}`;
-        }
-    } finally {
-        shareBtn.innerText = "코드 공유";
-        loading = false;
+  console.log(title.value, code.value);
+  e.preventDefault();
+  if (loading) return;
+  if (!title.value || !code.value) return;
+  try {
+    loading = true;
+    shareBtn.innerText = "로딩 중..";
+    const res = await customFetch("/codes", {
+      method: "POST",
+      body: {
+        title: title.value,
+        code: code.value,
+        field: "PYTHON",
+      },
+    });
+    if (res.ok && res.data) {
+      alert("코드 작성에 성공하였습니다.");
+      document.location.href = `/code?id=${res.data.id}`;
     }
-}
+  } finally {
+    shareBtn.innerText = "코드 공유";
+    loading = false;
+  }
+};
 
 shareBtn.addEventListener("click", handleSubmit);
