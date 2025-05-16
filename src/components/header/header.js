a8786221e908ef594ab7f4286ae8e1f446b7e65c
채근영 <chaulfe@gmail.com>
2025-05-17 02:11:54 +0900

    feat: 마이페이지 계정별 리다이렉션 기능 추가

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,3 +1,21 @@
 import headerHTML from "./header.html?raw";
+import { customFetch } from "/js/common.js";
+
+// document.addEventListener("DOMContentLoaded", async () => {
+//   const userName = document.querySelector(".user-name");
+//   const userNameText = document.createElement("div");
+//   const res = await customFetch(`/api/auth/info`, {
+//     method: "GET",
+//   });
+//   if (!res.ok) {
+//     console.error("HTTP 상태", res.status);
+//     const errorText = await res.text();
+//     console.error("서버 응답", errorText);
+//     return;
+//   }
+//   userNameText.className = "user-name-text";
+//   userNameText.innerHTML = `<a href="/user?id=${res.data.id}">${res.data.username}</a>`;
+//   userName.appendChild(userNameText);
+// });
 
 document.body.insertAdjacentHTML("afterbegin", headerHTML);
