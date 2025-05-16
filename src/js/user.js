834f278bf0972efe9043abe922fec5353409be97
채근영 <chaulfe@gmail.com>
2025-05-17 00:47:47 +0900

    feat: 유저 정보 불러오기 기능 추가

---
+++ src/js/user.js
@@ -0,0 +1,26 @@
+import { customFetch } from "/js/common.js";
+import hljs from "/js/highlight.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const studentName = document.querySelector(".school-name");
+  const username = document.getElementById("user-name");
+  const urlParams = new URLSearchParams(window.location.search);
+  const userId = urlParams.get("id");
+  try {
+    const res = await customFetch(`/users/${userId}`, {
+      method: "GET",
+    });
+    if (!res.ok) {
+      console.error("HTTP 상태", res.status);
+      const errorText = await res.text();
+      console.error("서버 응답", errorText);
+      return;
+    }
+    const data = await res.data;
+    const formattedStudentNumber = String(data.studentNumber).padStart(2, "0");
+    studentName.innerText = `${data.grade}${data.classNumber}${formattedStudentNumber}${data.name}`;
+    username.innerText = `${data.username}`;
+  } catch (e) {
+    console.error("API 요청 중 에러", e);
+  }
+});
