ea4af5d66f95f48e932b5fa3bfb71884f5a62934
채근영 <chaulfe@gmail.com>
2025-05-17 02:26:09 +0900

    fix: 올바른 경로로 리다이렉션 되도록 수정

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,21 +1,19 @@
 import headerHTML from "./header.html?raw";
 import { customFetch } from "/js/common.js";
 
-// document.addEventListener("DOMContentLoaded", async () => {
-//   const userName = document.querySelector(".user-name");
-//   const userNameText = document.createElement("div");
-//   const res = await customFetch(`/api/auth/info`, {
-//     method: "GET",
-//   });
-//   if (!res.ok) {
-//     console.error("HTTP 상태", res.status);
-//     const errorText = await res.text();
-//     console.error("서버 응답", errorText);
-//     return;
-//   }
-//   userNameText.className = "user-name-text";
-//   userNameText.innerHTML = `<a href="/user?id=${res.data.id}">${res.data.username}</a>`;
-//   userName.appendChild(userNameText);
-// });
+document.addEventListener("DOMContentLoaded", async () => {
+  const userNameText = document.querySelector(".user-name-text");
+  const res = await customFetch(`/api/auth/info`, {
+    method: "GET",
+  });
+  if (!res.ok) {
+    console.error("HTTP 상태", res.status);
+    const errorText = await res.text();
+    console.error("서버 응답", errorText);
+    return;
+  }
+  const data = await res.data;
+  userNameText.href = `/user?id=${data.id}`;
+});
 
 document.body.insertAdjacentHTML("afterbegin", headerHTML);
