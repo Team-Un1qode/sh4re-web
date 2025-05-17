b9eb4f91e681fd0e072283e0a98b4a795f1943e9
채근영 <chaulfe@gmail.com>
2025-05-17 15:43:07 +0900

    feat: 카테고리 변경 시 value값 반환

--- src/js/category.js
+++ src/js/category.js
@@ -2,6 +2,8 @@ import { customFetch } from "/js/common.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   const categoryAssignment = document.querySelector(".category-assignment");
+  const categoryClass = document.querySelector(".category-class");
+  const categorySort = document.querySelector(".category-sort");
   try {
     const res = await customFetch(`/assignments`, {
       method: "GET",
@@ -14,11 +16,24 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
     const data = await res.data;
     for (let i = 0; i < data.assignments.length; i++) {
-      const option = document.createElement("option");
       const assignment = data.assignments[i];
-      option.innerHTML = `<option value="${assignment.id}">${assignment.title}</option>`;
+      const option = document.createElement("option");
+      option.value = assignment.id;
+      option.textContent = assignment.title;
       categoryAssignment.appendChild(option);
     }
+    categoryAssignment.addEventListener("change", function () {
+      const assignmentValue = this.value;
+      console.log(`option.id = ${assignmentValue}`); // 과제 카테고리 변경 시 value 반환
+    });
+    categoryClass.addEventListener("change", function () {
+      const classValue = this.value;
+      console.log(`class.id = ${classValue}`);
+    });
+    categorySort.addEventListener("change", function () {
+      const sortValue = this.value;
+      console.log(`sort.id = ${sortValue}`);
+    });
   } catch (e) {
     console.error("API 요청 중 에러", e);
   }
