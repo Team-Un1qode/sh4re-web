5bcd5ff21af201a66793b1a42087a9b4b9759d10
채근영 <chaulfe@gmail.com>
2025-05-17 11:23:37 +0900

    feat: 유저별 작성한 코드 불러오기 기능 추가

--- src/js/user.js
+++ src/js/user.js
@@ -20,6 +20,34 @@ document.addEventListener("DOMContentLoaded", async () => {
     const formattedStudentNumber = String(data.studentNumber).padStart(2, "0");
     studentName.innerText = `${data.grade}${data.classNumber}${formattedStudentNumber}${data.name}`;
     username.innerText = `${data.username}`;
+
+    const codeList = await data.codeList;
+    if (codeList) {
+      for (let i = 0; i < codeList.length; i++) {
+        const code = codeList[i];
+        const article = document.createElement("article");
+        article.className = "post-list-box";
+        article.innerHTML = `
+                <a href="/code?id=${code.id}" class="detail-page">
+                <div class="code-text">
+                  <pre><code class="language-python">${code.code}</code></pre>
+                </div>
+                <div class="code-information">
+                  <div class="info-box">
+                    <p class="title">${code.title}</p>
+                    <p class="student-info">${data.grade}${data.classNumber}${formattedStudentNumber}${data.name}(${data.username})</p>
+                  </div>  
+                  <div class="like-box">
+                    <img src="/like.svg" alt="likeIcon" width="23px" />
+                    <p className="like-count">${code.likes}</p>
+                  </div>
+                </div>
+                </a>
+                `;
+        document.getElementById("post-list-container").appendChild(article);
+      }
+      hljs.highlightAll();
+    }
   } catch (e) {
     console.error("API 요청 중 에러", e);
   }
