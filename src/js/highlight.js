b08712c83808a3df17946bf2833f6749d17ae8d8
채근영 <chaulfe@gmail.com>
2025-05-18 01:22:03 +0900

    refactor: 사용하지 않는 라이브러리 제거

--- src/js/highlight.js
+++ src/js/highlight.js
@@ -1,13 +1,7 @@
 import hljs from "highlight.js/lib/core";
 import python from "highlight.js/lib/languages/python";
-import xml from "highlight.js/lib/languages/xml";
-import javascript from "highlight.js/lib/languages/javascript";
-import css from "highlight.js/lib/languages/css";
 import "highlight.js/styles/atom-one-dark.css";
 
 hljs.registerLanguage("python", python);
-hljs.registerLanguage("html", xml);
-hljs.registerLanguage("javascript", javascript);
-hljs.registerLanguage("css", css);
 
 export default hljs;
