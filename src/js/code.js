93e7d669f6a2ab8eef8745a4e9a8fd5c274e7adc
채근영 <chaulfe@gmail.com>
2025-05-17 10:29:30 +0900

    feat: 전체화면 확대/축소 버튼 추가

--- src/js/code.js
+++ src/js/code.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const descriptionBox = document.querySelector(".description");
   const userInformationBox = document.querySelector(".user-info");
   const codeBox = document.querySelector(".code-text");
+  const fullScreenIcon = document.getElementById("full-screen-icon");
 
   try {
     const res = await customFetch(`/codes/${postId}`, {
@@ -52,6 +53,13 @@ document.addEventListener("DOMContentLoaded", async () => {
         codeBox.requestFullscreen();
       } else {
         document.exitFullscreen();
+      }
+    });
+    document.addEventListener("fullscreenchange", () => {
+      if (document.fullscreenElement) {
+        fullScreenIcon.innerHTML = `<path d="M160 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l96 0c17.7 0 32-14.3 32-32l0-96zM32 320c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0 0 64c0 17.7 14.3 32 32 32s32-14.3 32-32l0-96c0-17.7-14.3-32-32-32l-96 0zM352 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7 14.3 32 32 32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0 0-64zM320 320c-17.7 0-32 14.3-32 32l0 96c0 17.7 14.3 32 32 32s32-14.3 32-32l0-64 64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0z"/>`;
+      } else {
+        fullScreenIcon.innerHTML = `<path d="M32 32C14.3 32 0 46.3 0 64l0 96c0 17.7 14.3 32 32 32s32-14.3 32-32l0-64 64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7 14.3 32 32 32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0 0-64zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0 0 64c0 17.7 14.3 32 32 32s32-14.3 32-32l0-96c0-17.7-14.3-32-32-32l-96 0zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l96 0c17.7 0 32-14.3 32-32l0-96z"/>`;
       }
     });
   } catch (e) {
