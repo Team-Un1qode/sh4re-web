435dd72add81223e749b7e7df08979edbe322957
채근영 <chaulfe@gmail.com>
2025-05-18 01:24:20 +0900

    refactor: 옵셔널 체이닝 사용

--- src/js/status.js
+++ src/js/status.js
@@ -1,22 +1,22 @@
 const createAcc = document.querySelector(".create-account");
 const loginForm = document.querySelector(".login-form");
 const signUpForm = document.querySelector(".signup-form");
-const modalConontent = document.querySelector(".modal-content");
+const modalContent = document.querySelector(".modal-content");
 let currentStatus = "로그인";
 
 if (createAcc) {
   createAcc.addEventListener("click", () => {
     if (currentStatus === "로그인") {
-      loginForm.classList.add("hidden");
-      signUpForm.classList.remove("hidden-signup");
+      loginForm?.classList.add("hidden");
+      signUpForm?.classList.remove("hidden-signup");
       createAcc.innerHTML = "계정이 있으신가요?";
-      modalConontent.classList.add("expanded");
+      modalContent?.classList.add("expanded");
       currentStatus = "회원가입";
     } else if (currentStatus === "회원가입") {
-      loginForm.classList.remove("hidden");
-      signUpForm.classList.add("hidden-signup");
+      loginForm?.classList.remove("hidden");
+      signUpForm?.classList.add("hidden-signup");
       createAcc.innerHTML = "아직 계정이 없으신가요?";
-      modalConontent.classList.remove("expanded");
+      modalContent?.classList.remove("expanded");
       currentStatus = "로그인";
     }
   });
