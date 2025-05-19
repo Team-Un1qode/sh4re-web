822af3aca45c9f596ac63698dbd7949ce544c372
chaeyn <chaulfe@gmail.com>
2025-05-19 09:26:49 +0900

    fix: Teacher페이지에서만 Teacher 아이콘 보이도록 수정

---
+++ src/components/teacher-header/teacherHeader.js
@@ -0,0 +1,20 @@
+import headerHTML from "./teacher-header.html?raw";
+import { customFetch, getCookie } from "/js/common.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  if (getCookie("accessToken") == "") return;
+  const userNameText = document.querySelector(".user-name-text");
+  const res = await customFetch(`/api/auth/info`, {
+    method: "GET",
+  });
+  if (!res.ok) {
+    console.error("HTTP 상태", res.status);
+    const errorText = await res.text();
+    console.error("서버 응답", errorText);
+    return;
+  }
+  const data = await res.data;
+  userNameText.href = `/user?id=${data.id}`;
+});
+
+document.body.insertAdjacentHTML("afterbegin", headerHTML);
